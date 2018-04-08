c2fa750340ed98f9c1035d956f6ff9d4dfa7ea6f
ohrha <trigramthree@gmail.com>
2018-04-08 15:50:11 +1200

    INITIAL-COMMIT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 var app = express()
 
-app.use(express.static(__dirname,"dist"));
+app.use(express.static(path.join(__dirname,"dist")));
 app.use(cors());
 app.get("*" ,(req,res)=>{
 
