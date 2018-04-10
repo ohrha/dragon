09e26c00665863c88a8e93784ce734bc631b8822
ohrha <trigramthree@gmail.com>
2018-04-10 12:43:45 +1200

    FIX-MATERIALIZE-ICONS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, ActivatedRoute, Params, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+/*
+const appRoutes = [
+
+  { path: '', component:HomeComponent}
 
 
+]
+*/
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
--- src/assets/main.js
+++ src/assets/main.js
@@ -1,17 +1,1 @@
-$(document).ready(()=>{
-
-console.log("I''m ready");
-const droppable = new Draggable.Droppable(document.querySelectorAll("ul"), {
-  draggable: "li",
-  droppable: "ul"
-});
-
-droppable.on("droppable:over", function() {
-  $("ul").removeClass("draggable-droppable--occupied");
-});
-droppable.on("droppable:out", () => console.log("droppable:out"));
-
-
-
-
-})
+console.log("i'm ready")
